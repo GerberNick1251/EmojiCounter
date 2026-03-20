d0329bd437606204fe9fc38bf7021d9c7af8dea5
Nicholas Gerber <nzg0069@auburn.edu>
2026-03-20 18:13:03 -0400

    Created the app

--- EmojiCounter/ContentView.swift
+++ EmojiCounter/ContentView.swift
@@ -7,18 +7,45 @@
 
 import SwiftUI
 
+var emojis : [String] = ["\u{1F923}", "\u{1F61C}", "\u{1F600}", "\u{1F929}", "\u{1F602}"]
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(emojis, id:\.self) { emoji in
+                    CounterView(emoji: emoji)
+                        .listRowInsets(EdgeInsets(top: 0, leading: 40, bottom: 0, trailing: 40))
+                        .frame(height: 80)
+                }
+            }
+            .listStyle(.insetGrouped)
+            .navigationTitle(Text("Emoji Counter"))
+            .navigationBarTitleDisplayMode(.inline)
+            
         }
-        .padding()
     }
 }
 
+struct CounterView: View {
+    @State private var count: Int = 0
+    var emoji : String
+    var body: some View {
+        HStack {
+            Text("\(emoji)  Counter:  \(count)")
+            Spacer()
+            Button("+") {
+                count += 1
+            }
+            Button("\u{2014}") {
+                count -= 1
+            }
+        }
+        .buttonStyle(.bordered)
+        .buttonBorderShape(.roundedRectangle(radius: 10))
+        .foregroundColor(.red)
+    }
+}
 #Preview {
     ContentView()
 }
