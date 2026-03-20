0128f098bb27e44f3d420f37a516488e2f6c7a7d
Nicholas Gerber <nzg0069@auburn.edu>
2026-03-20 16:28:31 -0400

    Initial Commit

---
+++ EmojiCounter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EmojiCounter
+//
+//  Created by Nick.Gerber.2 on 2026-03-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EmojiCounter/EmojiCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiCounterApp.swift
+//  EmojiCounter
+//
+//  Created by Nick.Gerber.2 on 2026-03-20.
+//
+
+import SwiftUI
+
+@main
+struct EmojiCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
